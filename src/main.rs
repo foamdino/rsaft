1c8422f76c6db8a977afaeed84a120e60b31933b
Kev Jackson <kevin.jackson@thehutgroup.com>
2019-03-15 10:56:05 +0000

    starting on playing with raft

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+
+struct Append {
+    sender: String,
+    receiver: String
+}
+
+struct RequestVote {
+    sender: String,
+    receiver: String
+}
+
+struct Response {
+    sender: String,
+    receiver: String
+}
+
+fn main() {
+    println!("Hello, world!");
+}
